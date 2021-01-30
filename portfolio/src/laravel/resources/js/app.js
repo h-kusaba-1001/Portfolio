168efb796fac6fb3991a160ace487fb2d0723403
h-kusaba-1001 <jojo1889jojo@gmail.com>
2021-01-30 20:39:10 +0900

    vuetifyを導入

--- portfolio/src/laravel/resources/js/app.js
+++ portfolio/src/laravel/resources/js/app.js
@@ -8,6 +8,8 @@ require('./bootstrap');
 
 window.Vue = require('vue').default;
 
+import vuetify from './lib/vuetify'
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -29,4 +31,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    vuetify: vuetify,
 });
---
+++ portfolio/src/laravel/resources/js/lib/vuetify.js
@@ -0,0 +1,8 @@
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+window.Vue = require('vue').default;
+
+window.Vue.use(Vuetify);
+const opts = {}
+
+export default new Vuetify(opts)
