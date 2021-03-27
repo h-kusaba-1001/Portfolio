8d0c72a984b98f140e756d7a360cb41eb1df0d81
h-kusaba-1001 <jojo1889jojo@gmail.com>
2021-03-28 01:31:48 +0900

    管理画面bladeの整理
    ・Topを簡潔に
    ・雑にブログ記事のリンクを設置

--- portfolio/src/laravel/resources/views/admin/home.blade.php
+++ portfolio/src/laravel/resources/views/admin/home.blade.php
@@ -5,16 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">管理者 Dashboard</div>
-
+                <div class="card-header">Admin Dashboard</div>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
                     You are logged in as 管理者!
+                    <a class="btn btn-primary btn-block" role="button" href="{{ route('admin.article.index') }}">ブログ</a>
                 </div>
             </div>
         </div>
