3f53eb17dcba5c834c72c8a843ca8e4b8e1fcb4f
h-kusaba-1001 <jojo1889jojo@gmail.com>
2021-05-02 21:24:27 +0900

    axiosでHTTPリクエストを送っていた不具合を解消

--- src/laravel/resources/js/lib/axios.js
+++ src/laravel/resources/js/lib/axios.js
@@ -1,6 +1,8 @@
 const axiosBase = require("axios");
 // eslint-disable-next-line no-unused-vars
 const axios = axiosBase.create({
+    // eslint-disable-next-line no-undef
+    baseURL: process.env.MIX_AXIOS_BASE_URL,
     headers: {
         "Content-Type": "application/json",
         "X-Requested-With": "XMLHttpRequest",
